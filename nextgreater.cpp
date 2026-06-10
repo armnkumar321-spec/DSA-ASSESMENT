2b99576008fab8d4f1998d72ab6f43baf5e8f303
AMAN KUMAR <armnkumar321@gmail.com>
2026-06-10 09:50:11 +0530

    Add files via upload

---
+++ nextgreater.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> arr = {2, 1, 3, 5, 4};
+    int n = arr.size();
+
+    vector<int> answer(n, -1);
+    stack<int> st;
+
+    for(int i = n - 1; i >= 0; i--) {
+
+        while(!st.empty() && st.top() <= arr[i]) {
+            st.pop();
+        }
+
+        if(!st.empty()) {
+            answer[i] = st.top();
+        }
+
+        st.push(arr[i]);
+    }
+
+    for(int x : answer) {
+        cout << x << " ";
+    }
+}
