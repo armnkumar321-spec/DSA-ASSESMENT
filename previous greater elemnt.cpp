df648589d1bf5ab118f04f13e84c21db521a3e03
AMAN KUMAR <armnkumar321@gmail.com>
2026-06-10 15:15:21 +0530

    Update previous greater elemnt.cpp

--- previous greater elemnt.cpp
+++ previous greater elemnt.cpp
@@ -5,7 +5,7 @@ int main() {
     vector<int> arr = {2, 1, 3, 5, 4};
     int n = arr.size();
 
-    vector<int> answer(n, -1);
+    vector<int> ans(n, -1);
     stack<int> st;
 
     for(int i = 0; i < n; i++) {
@@ -15,13 +15,13 @@ int main() {
         }
 
         if(!st.empty()) {
-            answer[i] = st.top();
+            ans[i] = st.top();
         }
 
         st.push(arr[i]);
     }
 
-    for(int x : answer) {
+    for(int x : ans) {
         cout << x << " ";
     }
 }
