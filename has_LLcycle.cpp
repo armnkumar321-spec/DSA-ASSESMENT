8a039f54a1c48275b25e038ecd2c6663a75b9593
AMAN KUMAR <armnkumar321@gmail.com>
2026-06-14 16:35:54 +0530

    Add files via upload

---
+++ has_LLcycle.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+public:
+    int data;
+    Node* next;
+    Node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+
+class List{
+    Node* head;
+    Node* tail;
+public:
+    List(){
+        head=tail=NULL;
+    }
+
+
+
+
+    void Push_front(int val){
+        Node* newNode=new Node(val);
+        if(head==NULL){
+            head=tail=newNode;
+            return;
+        }
+        else{
+        newNode->next=head;
+        head=newNode;
+    }
+    
+
+}
+bool hascycle() {
+        Node* slow=head;
+        Node* fast=head;
+        while(fast&&fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
+            return true;
+        }
+        
+            return false;
+        
+        
+    }
+
+    
+
+
+
+
+
+    void printLL(){
+        Node* temp=head;
+        while(temp!=NULL){
+            cout<<temp->data<<"->";
+            temp=temp->next;
+        }
+        cout<<"null\n";
+    }
+
+};
+
+
+
+
+
+
+int main(){
+    List l;
+    l.Push_front(10);
+    l.Push_front(20);
+    l.Push_front(30);
+    l.Push_front(40);
+    l.Push_front(50);
+
+    l.printLL();
+    if(l.hascycle()){
+        cout<<"true";
+    }
+    else{
+        cout<<"false";
+    }
+    
+
+
+    
+
+    
+
+
+}
